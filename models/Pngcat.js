81ce0333b91f423a16ca038ec9e919c84a9811eb
hengyuni <hengyuni@gmail.com>
2021-05-19 22:32:21 -0400

    crud backend for Pngcat

--- models/Pngcat.js
+++ models/Pngcat.js
@@ -1,22 +1,20 @@
-const { Schema, model } = require('mongoose');
-const SBOL_GlyphSchema = require('./SBOL_Glyph');
+const { Schema, model } = require("mongoose");
+const SBOL_GlyphSchema = require("./SBOL_Glyph");
 
 // Removed authors & DNA seq since api doesn't return it.
 const PngcatSchema = new Schema({
+	// Symbols mapping to genes / functions
+	SBOL_Glyphs: [SBOL_GlyphSchema],
 
-  // Symbols mapping to genes / functions
-  SBOL_Glyphs: [SBOL_GlyphSchema],
+	/* ---- Keys ---- */
+	// ENA Foreign key
+	Seq_Accession: { type: String, required: true },
 
-  /* ---- Keys ---- */
-  // ENA Foreign key
-  Seq_Accession: { type: String, required: true },
+	// ENA Linked article key
+	Study_Accession: { type: String, required: true },
 
-  // ENA Linked article key
-  Study_Accession: { type: String, required: true },
-
-  // SBOL API url: http://{SBOL-VO-WS}/glyph/{ONTOLOGY_TERM}"}
-  'SBOL-VO-WS': { type: String, required: true },
-
+	// SBOL API url: http://{SBOL-VO-WS}/glyph/{ONTOLOGY_TERM}"}
+	// 'SBOL-VO-WS': { type: String, required: true },
 });
 
-module.exports = model('Pngcat', PngcatSchema);
+module.exports = model("Pngcat", PngcatSchema);
